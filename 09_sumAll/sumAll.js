38909a88696b1d84a9e44bb4aa0fde03ebf3d0c3
Sean-Murphy98 <92535432+Sean-Murphy98@users.noreply.github.com>
2025-08-25 03:23:51 -0700

    Completed exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    sum = 0;
+    if (num1 < 0 || !Number.isInteger(num1)|| num2 < 0 || !Number.isInteger(num2)){
+        return "ERROR"
+    }
+    let a = Math.min(...arguments)
+    let b = Math.max(...arguments)
+    for (let i=a; i<=b; i++){
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
