389b9eff6720ac88713278486fc5cb4b7d32027d
Sean-Murphy98 <92535432+Sean-Murphy98@users.noreply.github.com>
2025-08-25 02:41:48 -0700

    Problem 7 passed

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    let l = s.length
+    let a = ""
+    for (l; l>0; l--){
+        a += s[l-1]
+    }
+    return a
 };
 
 // Do not edit below this line
