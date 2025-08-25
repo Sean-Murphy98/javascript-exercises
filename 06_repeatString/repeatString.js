b746e70cdcecddd39abab15f4ba287cb0346f820
Sean-Murphy98 <92535432+Sean-Murphy98@users.noreply.github.com>
2025-08-25 02:30:46 -0700

    Problem 6 passed

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(stri, num) {
+    if (num < 0){
+        return "ERROR"
+    }
+    let a = ""
+    for (let i = 0; i<num; i++){
+        a += stri
+    }
+    return a
 };
 
 // Do not edit below this line
