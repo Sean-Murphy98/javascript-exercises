40bdff436acc3bfdb69fbeaecfc49f6eeb3d2d2c
Sean-Murphy98 <92535432+Sean-Murphy98@users.noreply.github.com>
2025-08-25 03:00:51 -0700

    completed assignment 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
 const removeFromArray = function() {
+    arr = arguments[0]
+    for (let i=1; i<arguments.length; i++){
+        arr = arr.filter((entry) => entry !== arguments[i]);
+    }
+    return arr
 };
 
 // Do not edit below this line
